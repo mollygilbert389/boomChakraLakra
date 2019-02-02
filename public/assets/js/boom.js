bd59ba93424df3c6ef226b49b4c9ec275884f8d6
Dustin Crawford <crawforddustins@yahoo.com>
2019-02-02 13:35:36 -0600

    HTML

---
+++ public/assets/js/boom.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+
+  console.log(instances)
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+    console.log(instances)
+  });
+
